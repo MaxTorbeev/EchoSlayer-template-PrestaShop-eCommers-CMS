f3f76fa55750106b41ebcc4a021776c5f3f15027
Maxim Torbeev <mxtsoftprow@yandex.ru>
2017-02-07 16:33:48 +0300

    migrate to webpack@2

--- _dev/webpack.config.js
+++ _dev/webpack.config.js
@@ -1,89 +1,91 @@
-/**
- * 2007-2016 PrestaShop
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@prestashop.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
- * versions in the future. If you wish to customize PrestaShop for your
- * needs please refer to http://www.prestashop.com for more information.
- *
- * @author    PrestaShop SA <contact@prestashop.com>
- * @copyright 2007-2016 PrestaShop SA
- * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
- * International Registered Trademark & Property of PrestaShop SA
- */
-var webpack = require('webpack');
-var path = require('path');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var webpack             = require('webpack'),
+    path                = require('path'),
+    ExtractTextPlugin   = require("extract-text-webpack-plugin");
 
-var plugins = [];
+var plugins             = [];
 
-var production = false;
+var production          = false;
 
-if (production) {
-    plugins.push(
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        })
-    );
-}
-
-plugins.push(
-    new ExtractTextPlugin(
-        path.join(
-            '..', 'css', 'theme.css'
-        )
-    )
-);
+const IS_DEV = true;
 
 module.exports = {
+
     entry: [
         './js/theme.js'
     ],
+
     output: {
         path: '../assets/js',
         filename: 'theme.js'
     },
+
     module: {
-        loaders: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loaders: ['babel-loader']
-        }, {
-            test: /\.scss$/,
-            loader: ExtractTextPlugin.extract(
-                "style",
-                "css?sourceMap!postcss!sass?sourceMap"
-            )
-        }, {
-            test: /.(png|woff(2)?|eot|ttf|svg)(\?[a-z0-9=\.]+)?$/,
-            loader: 'file-loader?name=../css/[hash].[ext]'
-        }, {
-            test: /\.css$/,
-            loader: "style-loader!css-loader!postcss-loader"
-        }]
+        rules: [
+            {
+                test: /\.scss$/,
+
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        "css-loader",
+                        "sass-loader"
+                    ]
+                })
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: { modules: true }
+                    }
+                ],
+            },
+        ]
     },
-    postcss: function() {
-        return [require('postcss-flexibility')];
-    },
+
     externals: {
         prestashop: 'prestashop'
     },
+
     devtool: 'source-map',
-    plugins: plugins,
+
+    plugins: [
+        new ExtractTextPlugin({
+            filename: '../css/theme.css',
+            allChunks: true
+        })
+    ],
+
     resolve: {
-        extensions: ['', '.js', '.scss']
+        extensions: ['*', '.js', '.scss'],
+        modules: [path.resolve(__dirname, './src'), 'node_modules']
     }
 };
+
+
+module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.ProvidePlugin({
+            jQuery          : 'jquery',
+            $               : 'jquery',
+            jquery          : 'jquery',
+            'window.jQuery' : 'jquery'
+        })
+]);
+
+
+//if (process.env.NODE_ENV === 'production'){
+//    module.exports.plugins.push(
+//
+//        new webpack.optimize.UglifyJsPlugin({
+//
+//            sourcemap: true,
+//
+//            compress: {
+//                warnings: false
+//            }
+//        })
+//
+//    )
+//}
